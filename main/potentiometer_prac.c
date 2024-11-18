d35c531f6ed7ae0d16d51cfd33ee34de3985c4cd
acousticCh <hassenr@protonmail.com>
2024-11-18 12:17:59 -0500

    figured out oneshot only using documentation!

---
+++ main/potentiometer_prac.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "esp_log.h"
+
+
+#define ADC1_CHAN0 ADC_CHANNEL_0
+#define ADC_ATTEN ADC_ATTEN_DB_12
+
+const static char *TAG = "EXAMPLE";
+
+static int adc_raw[2][10];
+// static int voltage[2][10];
+
+
+void app_main(void)
+{
+//----------------create adc unit handle----------------------|
+
+	adc_oneshot_unit_handle_t adc1_handle;
+	adc_oneshot_unit_init_cfg_t init_config1 =
+			{
+				.unit_id = ADC_UNIT_1,
+				.ulp_mode = ADC_ULP_MODE_DISABLE,
+			}; // end of init_config1
+
+	adc_oneshot_new_unit(&init_config1, &adc1_handle);
+
+ //-----------------------------------------------------------|
+
+
+//-------------------unit configuration-----------------------|
+
+	adc_oneshot_chan_cfg_t chan_config =
+			{
+				.bitwidth = ADC_BITWIDTH_DEFAULT,
+				.atten = ADC_ATTEN,
+			};// end of chan_config
+
+	adc_oneshot_config_channel(adc1_handle, ADC1_CHAN0, &chan_config);
+
+//-------------------------------------------------------------|
+
+
+	adc_oneshot_read(adc1_handle, ADC1_CHAN0, &adc_raw[0][0]);
+	ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d",ADC_UNIT_1 + 1, ADC1_CHAN0, adc_raw[0][0]);
+}
